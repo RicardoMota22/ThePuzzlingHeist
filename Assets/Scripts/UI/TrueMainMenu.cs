910d7bc431e849ff50b0625b19d2a4e6aef8ffbf
bernabarros <bernardobarros.work@gmail.com>
2026-01-06 20:12:34 +0000

    Add overview screen to mainmenu scene

--- Assets/Scripts/UI/TrueMainMenu.cs
+++ Assets/Scripts/UI/TrueMainMenu.cs
@@ -3,10 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class TrueMainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject overviewMenu;
+    private bool inOverview;
     void Start()
     {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+        inOverview = false;
+        mainMenu.SetActive(true);
+        overviewMenu.SetActive(false);
+    }
+    void Update()
+    {
+        if(Input.GetButtonDown("Interact") && inOverview)
+        {
+            StartGame();
+        }
     }
     public void StartGame()
     {
@@ -17,5 +30,11 @@ public class TrueMainMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Game Quit");
+    }
+    public void GoToOverview()
+    {
+        mainMenu.SetActive(false);
+        overviewMenu.SetActive(true);
+        inOverview = true;
     }
 }
