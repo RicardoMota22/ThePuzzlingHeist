ddf23b71d2189c6a3fb157864e034a3543ff7582
bernabarros <bernardobarros.work@gmail.com>
2026-01-06 16:20:58 +0000

    Add quit confirmation to pause menu

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -5,31 +5,45 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenuUI;
+    public GameObject quitConfirmUI;
 
     private bool isPaused = false;
 
     void Awake()
     {
         pauseMenuUI.SetActive(false);
+        quitConfirmUI.SetActive(false);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            if(quitConfirmUI.activeSelf)
+            {
+                CancelQuit();
+                return;
+            }
             if (isPaused)
+            {
                 Resume();
+            }
             else
-            ShowBookContent.isReading = true;
+            {
+                ShowBookContent.isReading = true;
                 Pause();
+            }
         }
     }
 
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        quitConfirmUI.SetActive(false);
+
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
         Time.timeScale = 1f;
         isPaused = false;
         ShowBookContent.isReading = false;
@@ -38,16 +52,29 @@ public class PauseMenu : MonoBehaviour
     void Pause()
     {
         pauseMenuUI.SetActive(true);
+        quitConfirmUI.SetActive(false);
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
         Time.timeScale = 0f;
         isPaused = true;
     }
 
-    public void QuitToMenu()
+    public void OpenQuitConfirmation()
+    {
+        pauseMenuUI.SetActive(false);
+        quitConfirmUI.SetActive(true);
+    }
+
+    public void ConfirmQuit()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
+    }
+    public void CancelQuit()
+    {
+        pauseMenuUI.SetActive(true);
+        quitConfirmUI.SetActive(false);
+        isPaused = true;
     }
 }
 
